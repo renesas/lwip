4fd1f636b68fb58734b1854e04fe4f62535763cd
Canh Phan <canh.phan.xb@renesas.com>
2025-02-24 11:20:31 +0700

    Fix a finding

--- src/apps/altcp_tls/altcp_tls_mbedtls.c
+++ src/apps/altcp_tls/altcp_tls_mbedtls.c
@@ -680,7 +680,11 @@ altcp_tls_set_session(struct altcp_pcb *conn, struct altcp_tls_session *session)
   if (session && conn && conn->state) {
     altcp_mbedtls_state_t *state = (altcp_mbedtls_state_t *)conn->state;
     int ret = -1;
+#if defined(MBEDTLS_HAVE_TIME)
+    if (session->data.MBEDTLS_PRIVATE(start))
+#else
     if (session->data.MBEDTLS_PRIVATE(id_len))
+#endif
       ret = mbedtls_ssl_set_session(&state->ssl_context, &session->data);
     return ret < 0 ? ERR_VAL : ERR_OK;
   }
